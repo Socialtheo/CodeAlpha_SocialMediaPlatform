5400f2403f78fcde82e32d03aabc0a5ccf9934d5
Ohene B.Atuahene <ohenebatuahene@gmail.com>
2026-08-24 16:01:31 +0000

    Update urls.py

--- social/urls.py
+++ social/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('feed/', views.feed_view, name='social_feed'),
+    path('', views.feed_view, name='social_feed'),
     path('like/<int:post_id>/', views.like_post, name='social_like_post'),
+    path('delete-post/<int:post_id>/', views.delete_post, name='delete_post'),
     path('comment/<int:post_id>/', views.add_comment, name='social_add_comment'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='social_delete_comment'),
     path('profile/<str:username>/', views.profile_view, name='social_profile'),
     path('follow/<str:username>/', views.follow_user, name='social_follow_user'),
     path('register/', views.social_register, name='social_register'),
