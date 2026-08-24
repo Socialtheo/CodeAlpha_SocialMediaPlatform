12f4b0eb34b188d1451bcc1ac9cd5d85a047ee74
Ohene B.Atuahene <ohenebatuahene@gmail.com>
2026-08-24 16:04:48 +0000

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
-    path('social/', include('social.urls')), 
+    path('', include('social.urls')), 
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
